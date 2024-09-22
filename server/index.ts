0dd7314995b7e274ebeb42d647a1f866304264ac
Rohank1442 <rohankumar1442@gmail.com>
2024-09-22 19:13:51 +0530

    cors

--- server/index.ts
+++ server/index.ts
@@ -9,21 +9,28 @@ dotenv.config();
 
 const app: Express = express();
 
-app.use(cors({
-    origin:["https://kanban-seven-blush.vercel.app/"],
-    methods:['POST','GET','HEAD','PUT','DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: ["https://kanban-seven-blush.vercel.app"],
+    methods: ["POST", "GET", "HEAD", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use(express.json({limit: "100mb"}));
-app.use(express.urlencoded({limit:"50mb", extended: true}));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use("/api", router);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.wto2koe.mongodb.net/test`).then(() => {
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.wto2koe.mongodb.net/test`
+  )
+  .then(() => {
     console.log("database connected");
     app.listen(8000, () => {
-        console.log("App running on 8000 port");
-    })
-}).catch((err: any) => {
+      console.log("App running on 8000 port");
+    });
+  })
+  .catch((err: any) => {
     console.log(err);
-})
+  });
